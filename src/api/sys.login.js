c0118b584dbec4aa300ee9aacb3a6f0575e890a8
liuyann <liuyan_wangyi@126.com>
2020-01-17 17:55:46 +0800

    ok

--- src/api/sys.login.js
+++ src/api/sys.login.js
@@ -1,7 +1,10 @@
 import request from '@/plugin/axios'
 
+var ZHX_BASE_URL = 'http://localhost:8081'
+
 export function AccountLogin (data) {
   return request({
+    baseURL: ZHX_BASE_URL,
     url: '/login',
     method: 'post',
     data
