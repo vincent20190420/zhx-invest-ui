a627ebfe340c49fa6679d90bb7a18760b376a5bd
liuyann <liuyan_wangyi@126.com>
2020-01-18 17:51:49 +0800

    ok

--- src/api/sys.login.js
+++ src/api/sys.login.js
@@ -1,12 +1,12 @@
 import request from '@/plugin/axios'
+import querystring from 'querystring'
 
 var ZHX_BASE_URL = 'http://localhost:8081'
 
 export function AccountLogin (data) {
   return request({
     baseURL: ZHX_BASE_URL,
-    url: '/login?username=admin&password=admin',
-    method: 'post',
-    data
+    url: '/oauth/token?grant_type=password&client_id=client&client_secret=secret&' + querystring.stringify(data),
+    method: 'post'
   })
 }
