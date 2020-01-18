79ad6580548d7b1733ef2b58415fb182d3871d49
liuyann <liuyan_wangyi@126.com>
2020-01-18 17:15:00 +0800

    ok

--- src/api/sys.login.js
+++ src/api/sys.login.js
@@ -5,7 +5,7 @@ var ZHX_BASE_URL = 'http://localhost:8081'
 export function AccountLogin (data) {
   return request({
     baseURL: ZHX_BASE_URL,
-    url: '/login',
+    url: '/login?username=admin&password=admin',
     method: 'post',
     data
   })
